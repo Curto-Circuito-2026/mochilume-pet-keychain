972c15d9e0d6c13e1d9bd9794f6277265d69392f
ArthurPalladino <arthur.palladino1@gmail.com>
2026-03-27 19:06:22 -0300

    FEAT: initial graphic framework base

---
+++ mochilume-code/include/HalConfig.h
@@ -0,0 +1,36 @@
+#ifndef HAL_CONFIG_H
+#define HAL_CONFIG_H
+
+#include <Arduino.h>
+
+// --- DISPLAY SETTINGS (GC9A01) ---
+#define TFT_RES      4
+#define TFT_CS       5
+#define TFT_DC       15
+#define TFT_MOSI     23
+#define TFT_SCLK     18
+#define TFT_BL       22
+
+#define SCREEN_WIDTH  240
+#define SCREEN_HEIGHT 240
+#define SCREEN_RADIUS 120
+#define SCREEN_CENTER 120
+
+// --- SYSTEM SETTINGS ---
+#define TARGET_FPS   60
+#define FRAME_DELAY  (1000 / TARGET_FPS)
+
+// --- LORA SX1268 (SPI) ---
+#define LORA_CS      14
+#define LORA_DIO1    26
+#define LORA_RST     12
+#define LORA_BUSY    13
+
+// --- STORAGE (SD CARD) ---
+#define SD_CS        2
+
+// --- SENSORS (I2C) ---
+#define MPU_SDA      21
+#define MPU_SCL      22
+
+#endif
--- mochilume-code/src/main.cpp
+++ mochilume-code/src/main.cpp
@@ -1,37 +1,31 @@
 #include <Arduino.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_GC9A01A.h>
-#include <SPI.h>
+#include "HalConfig.h"
+#include "DisplayManager.h"
 
-
-#define TFT_RES  4    //TFT_RES
-#define TFT_CS   5    //TFT_CS
-#define TFT_DC   15   //TFT_A0
-#define TFT_MOSI 23   //TFT_SDA
-#define TFT_SCLK 18   //TFT_SCK
-#define SCREEN_WIDTH 240
-#define SCREEN_HEIGHT 240
-#define FPS 60
-
-Adafruit_GC9A01A screen(TFT_CS, TFT_DC);
-
-int y = 0;
+DisplayManager* display;
 
 void setup() {
-  Serial.begin(115200);
-  screen.begin();
-  screen.fillScreen(GC9A01A_BLACK);
-
+    Serial.begin(115200);
+    delay(2000); //delay pra dar tempo do platformio abrir o serial e n perder os prints
+    display = DisplayManager::getInstance();
+    display->begin();
+    Serial.println("Mochilume OS: Setup Finalizado.");
 }
 
+int squareSize = 60;
 void loop() {
-  int oldY = y;
-  y += 9.81;
-  if (y>=240+55)
-  {
-    y=0;
-  }
-  screen.fillRect(100,oldY,50,55,GC9A01A_BLACK);
-  screen.fillRect(100,y,50,55,GC9A01A_RED);
-  delay(100);
+    static float y = 0;
+    float oldY = y;
+    
+    
+    y += 5.0f; 
+    if (y >= SCREEN_HEIGHT + squareSize) y = -squareSize;
+
+    Adafruit_GC9A01A* tft = display->getTFT();
+
+    tft->fillRect(90, (int)oldY, squareSize, squareSize, GC9A01A_BLACK);
+    //display->clear();
+    tft->fillRect(90, (int)y, squareSize, squareSize, GC9A01A_RED);
+
+    delay(FRAME_DELAY);
 }
