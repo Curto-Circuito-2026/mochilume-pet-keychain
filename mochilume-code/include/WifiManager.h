230c518d9f8d103ff4ba798b2c3e671819b29abe
Marlon Correia Bomfim Junior <42979884+marlaoza@users.noreply.github.com>
2026-05-25 13:59:47 -0300

    FEAT: WifiManager

---
+++ mochilume-code/include/WifiManager.h
@@ -0,0 +1,33 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+#include <Arduino.h>
+#include <future>
+
+#define CONNECTION_TIMEOUT 10000
+
+enum HTTPMETHOD { 
+    GET,
+    POST
+};
+class WifiManager {
+private:
+    static WifiManager* _instance;
+    
+
+    WifiManager();
+
+public:
+    static WifiManager* getInstance();
+
+    bool isConnected;
+
+    std::future<String> Fetch(String URI, HTTPMETHOD method, const String& payload);
+
+    std::future<String> Fetch(String URI);
+
+    std::vector<String> GetAvaliableWifis();
+    bool Connect(const String SSID, const String& password);
+};
+
+#endif
